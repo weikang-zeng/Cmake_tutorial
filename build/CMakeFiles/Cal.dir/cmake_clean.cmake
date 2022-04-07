7594e8d2abd21a8a8caac576d7f099c1af6c02c0
weikang-zeng <mr.gangge@hotmail.com>
2022-04-07 10:38:40 +0200

    test git

---
+++ build/CMakeFiles/Cal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cal.dir/src/calculatesqrt.cpp.o"
+  "Cal"
+  "Cal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
